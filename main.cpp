97acba8d10511af08b65465cc083147d1197ee45
Łukasz Nitka <lukasnit@wp.pl>
2021-06-14 17:46:31 +0200

    const

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
 #include "bd.hpp"
-#include 
+
 
 int main() {
-    Student student1;
-    student1.setName("Jan");
+    Student student1("Jan","Nowak","Poznan",4532,92071645517,'M');
+    
     std::cout << "student 1 name: " << student1.getName() << '\n';
+    std::cout << "student 1 surname: " << student1.getSurname() << '\n';
+    std::cout << "student 1 address: " << student1.getAddress() << '\n';
+    std::cout << "student 1 gender: " << student1.getGender() << '\n';
 }
