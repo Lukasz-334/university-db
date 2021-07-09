3286b425baecb706d87de342394c7b372c52f24b
Łukasz Nitka <lukasnit@wp.pl>
2021-07-09 19:18:01 +0200

    func. Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,1 +1,56 @@
 #include "Person.hpp"
+
+void Person::setName(const std::string& name) {
+    name_ = name;
+}
+
+void Person::setSurname(const std::string& surname) {
+    surname_ = surname;
+}
+
+void Person::setAddress(const std::string& address) {
+    address_ = address;
+}
+
+void Person::setIndex(const std::string& index) {
+    index_ = index;
+}
+
+void Person::setPesel(const std::string& pesel) {
+    pesel_ = pesel;
+}
+
+void Person::setGender(const std::string& gender) {
+    gender_ = gender;
+}
+void Person::setEarnings(const size_t& earnings) {
+    earnings_ = earnings;
+}
+
+std::string Person::getName() const {
+    return name_;
+}
+
+std::string Person::getSurname() const {
+    return surname_;
+}
+
+std::string Person::getAddress() const {
+    return address_;
+}
+
+std::string Person::getIndex() const {
+    return index_;
+}
+
+std::string Person::getPesel() const {
+    return pesel_;
+}
+
+std::string Person::getGender() const {
+    return gender_;
+}
+
+size_t Person::getEarnings() const{
+    return earnings_;
+}
--- Person.hpp
+++ Person.hpp
@@ -1,1 +1,38 @@
 #pragma once
+//#include "student.hpp"
+#include <iostream>
+#include <string>
+class Person{
+ 
+public:
+    std::string name_;
+    std::string surname_;
+    std::string address_;
+    std::string index_;
+    std::string pesel_;
+    std::string gender_; 
+    size_t earnings_;
+
+    Person(void);
+    virtual ~Person();
+
+
+    void setName(const std::string&);
+    void setSurname(const std::string&);
+    void setAddress(const std::string&);
+    void setIndex(const std::string&);
+    void setPesel(const std::string&);
+    void setGender(const std::string&);
+    void setEarnings(const size_t&);
+
+    std::string getName() const;
+    std::string getSurname() const;
+    std::string getAddress() const;
+    std::string getIndex() const;
+    std::string getPesel() const;
+    std::string getGender() const;
+    size_t getEarnings() const;
+
+    
+};
+
